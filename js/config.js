43fb23cc2bb0e432b9b6623d90cc91952fba8bc2
tb-miao <2307057760@qq.com>
2025-07-04 20:21:48 +0800

    2025-07-04-20-21修复BUG-imgerror

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ const announcementConfig = {
     popupDelay: 2000, // 页面加载后延迟显示弹出提示的时间（毫秒）
     popupDismissTime: 3600000, // 用户确认后不再显示的时间（毫秒），例如 1 小时
     confirmButtonText: "确认", // 确认按钮文本
-    dismissButtonText: "稍后再说", // 稍后再说按钮文本
+    dismissButtonText: "知道了", // 稍后再说按钮文本
     redirectUrl: "https://github.com/acanyo/handsomeHome", // 跳转地址
     redirectFlag: true // 是否启用确认跳转
 }; 
