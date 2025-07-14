826bc8aaf68efaf4c66301672855999fc40f3cc4
tb-miao <2307057760@qq.com>
2025-07-14 10:09:13 +0800

    ...

--- js/cnm.sb_copy.js
+++ js/cnm.sb_copy.js
@@ -16,3 +16,29 @@
 		}
 
 }))
+document.addEventListener('contextmenu', function(event) {
+
+    event.preventDefault();
+
+
+});
+// 禁止右键菜单
+document.oncontextmenu = function(){ return false; };
+// 禁止文字选择
+document.onselectstart = function(){ return false; };
+// 禁止复制
+document.oncopy = function(){ return false; };
+// 禁止剪切
+document.oncut = function(){ return false; };
+// 禁止粘贴
+document.onpaste = function(){ return false; };
+// 禁止拖放
+document.ondragstart = function(){ return false; };
+// 禁止F12调试
+document.onkeydown = function(event){
+    if(event.keyCode == 123){
+        return false;
+    }
+};
+// 禁止保存页面
+document.onbeforeunload = function(){ return false; };
