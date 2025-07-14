c24c0b30ef29095c8c5cd55e358165c3c6a1dcea
tb-miao <2307057760@qq.com>
2025-07-14 17:22:43 +0800

    2025-07-14-17-22更新awa

--- js/cnm.sb_judge.js
+++ js/cnm.sb_judge.js
@@ -1,10 +1,9 @@
 // 正在前往星球 - 页面展示切换
 $(document).ready(function(){
-	if(window.screen.width > window.screen.height){	
-		$("#pc").show();
-		$("#mp").hide();
-	}else{
-		$("#pc").hide();
-		$("#mp").show();
-	}
-})
+	$("#pc").show();
+});
+function closeWebNotice() {
+    document.querySelector('.web_notice').remove();
+};
+
+
